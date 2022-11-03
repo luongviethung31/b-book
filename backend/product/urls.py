a89a78f2006f26bfee44d5c132ddc25f81a57536
Luong Mau Viet Hung <luongmauviethung31.01@gmail.com>
2022-11-03 20:21:29 +0700

    done user

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -10,11 +10,11 @@ from .views import (
 )
 
 urlpatterns = [
-    path('genres/<str:slug>/', RetrieveUpdateDeleteGenreView.as_view(), name='RetrieveUpdateDeleteGenre'),
-    path('genres/', ListCreateGenreView.as_view(), name='ListCreateGenre'),
-    path('authors/<str:slug>/', RetrieveUpdateDeleteAuthorView.as_view(), name='RetrieveUpdateDeleteAuthor'),
-    path('authors/', ListCreateAuthorView.as_view(), name='ListCreateAuthor'),
-    path('books/<str:slug>/', RetrieveUpdateDeleteBookView.as_view(), name='RetrieveUpdateDeleteBook'),
-    path('books/', ListCreateBookView.as_view(), name='ListCreateBook'),
+    path('genres/<str:slug>', RetrieveUpdateDeleteGenreView.as_view(), name='RetrieveUpdateDeleteGenre'),
+    path('genres', ListCreateGenreView.as_view(), name='ListCreateGenre'),
+    path('authors/<str:slug>', RetrieveUpdateDeleteAuthorView.as_view(), name='RetrieveUpdateDeleteAuthor'),
+    path('authors', ListCreateAuthorView.as_view(), name='ListCreateAuthor'),
+    path('books/<str:slug>', RetrieveUpdateDeleteBookView.as_view(), name='RetrieveUpdateDeleteBook'),
+    path('books', ListCreateBookView.as_view(), name='ListCreateBook'),
 ]
  
--- backend/user/views.py
+++ backend/user/views.py
@@ -4,7 +4,6 @@ from rest_framework import (
     status,
     response,
 )
-
 from .models import (
     User
 )
@@ -14,29 +13,57 @@ from .serializers import (
     UserLoginSerializer
 )
 
+from rest_framework.authtoken.models import Token
+from django.contrib import auth
+
 
 class RegisterView(views.APIView):
+    permission_classes = [permissions.AllowAny]
     def post(self, request, *args, **kwargs):
         # Register
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            return response.Response(data=serializer.data, status=status.HTTP_201_CREATED)
+            serializer.save()
+            return response.Response({"message": "Register successful"}, status=status.HTTP_201_CREATED)
         return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class LoginView(views.APIView):
+    permission_classes = [permissions.AllowAny]
     def post(self, request):
         # Login
-        serializer = UserLoginSerializer(data=request.data)
-        if serializer.is_valid():
-            return response.Response(serializer.token, status=status.HTTP_202_ACCEPTED )
-        return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            serializer = UserLoginSerializer(data=request.data)
+            if serializer.is_valid():
+                user = auth.authenticate(
+                    request,
+                    username=serializer.validated_data['username'],
+                    password=serializer.validated_data['password']
+                )
+                if (user):
+                    Token.objects.filter(user=user).delete()
+                    token = Token.objects.create(user=user)
+                    auth.login(request, user)
+                    return response.Response({
+                        "token": token.key
+                        }, status=status.HTTP_200_OK)
+                else:
+                    return response.Response({
+                        "error_message": "Email or password is incorrect"
+                        }, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return response.Response({
+                    "error_message": "Email or password is invalid"
+                    }, status=status.HTTP_400_BAD_REQUEST)
 
 class UserDetailView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
     def get(self, request):
         # Get user info
         try:
-            user = User.objects.get(id=request.user.id)
-            serializer = UserSerializer(data=user)
+            user = User.objects.get(pk=request.user.id)
+            serializer = UserSerializer(user)
             return response.Response(serializer.data, status=status.HTTP_200_OK) 
         except User.DoesNotExist:
             return response.Response(status=status.HTTP_404_NOT_FOUND)
@@ -53,8 +80,11 @@ class UserDetailView(views.APIView):
             return response.Response(status=status.HTTP_404_NOT_FOUND)
 
 class LogoutView(views.APIView):
-    def post(self, request):
-        pass
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request):
+        request.user.auth_token.delete()
+        auth.logout(request)
+        return response.Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class UserView(views.APIView):
